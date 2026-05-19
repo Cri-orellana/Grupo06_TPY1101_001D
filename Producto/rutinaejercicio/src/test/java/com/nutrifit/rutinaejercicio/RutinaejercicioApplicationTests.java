d37e93fb27ec66eec1dd43eadcff0ecf27513bc2
Cristian Orellana <cristianj.orellanaa@gmail.com>
2026-05-19 07:30:33 -0400

    Se añade rutinas

---
+++ Producto/rutinaejercicio/src/test/java/com/nutrifit/rutinaejercicio/RutinaejercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrifit.rutinaejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutinaejercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
